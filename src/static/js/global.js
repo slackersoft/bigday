9ad20a1cc1bbd41c7a04b02ce8910af44798367b
gvanhove <gregg@slackersoft.net>
2010-03-10 04:13:15 +0000

    add info stuff to map

--- src/static/js/global.js
+++ src/static/js/global.js
@@ -1,6 +1,15 @@
 (function ($) {
 	var bd = (window.bigday = {});
 	
+	var marker = function (latlng, title, infoEl)
+	{
+		var m = new GMarker(latlng, {title: title});
+		
+		m.bindInfoWindow($(infoEl).get(0));
+		
+		return m
+	}
+	
 	var initMaps = function ()
 	{
 		var el = $('#mapsHolder');
@@ -10,18 +19,13 @@
 			var map = new GMap2(el.get(0));
 			
 			var fcLatLng = new GLatLng(38.2997720, -122.6439860);
-			var sheratonLatLng = new GLatLng(38.2307480, -122.6128900);
-			var bwLatLng = new GLatLng(38.2483650, -122.6256590);
-			var sfoLatLng = new GLatLng(37.6152230, -122.3899790);
-			var oakLatLng = new GLatLng(37.7125281, -122.2130026);
-			var stsLatLng = new GLatLng(38.5105557, -122.8055680);
 			
-			map.addOverlay(new GMarker(fcLatLng));
-			map.addOverlay(new GMarker(sheratonLatLng));
-			map.addOverlay(new GMarker(bwLatLng));
-			map.addOverlay(new GMarker(sfoLatLng));
-			map.addOverlay(new GMarker(oakLatLng));
-			map.addOverlay(new GMarker(stsLatLng));
+			map.addOverlay(marker(fcLatLng, "Flying Cloud Farm", "fcinfo"));
+			map.addOverlay(marker(new GLatLng(38.2307480, -122.6128900), "Sheraton Petaluma", "sherinfo"));
+			map.addOverlay(marker(new GLatLng(38.2483650, -122.6256590), "Best Western Petaluma", "bwinfo"));
+			map.addOverlay(marker(new GLatLng(37.6152230, -122.3899790), "San Francisco International", "sfoinfo"));
+			map.addOverlay(marker(new GLatLng(37.7125281, -122.2130026), "Oakland International", "oakinfo"));
+			map.addOverlay(marker(new GLatLng(38.5105557, -122.8055680), "Sonoma International", "stsinfo"));
 			
 			map.setCenter(fcLatLng, 13);
 	        map.setUIToDefault();
